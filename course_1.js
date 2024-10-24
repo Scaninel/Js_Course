edf215e3357a8072e7949e21fb63fda46da2311f
S.Can İNEL <inelscan@gmail.com>
2024-10-24 07:58:09 +0300

    step_1

--- course_1.js
+++ course_1.js
@@ -98,3 +98,52 @@ let shoppingList = ["Milk", "Bread", "Apple", "Olive"];
 console.log(shoppingList.length);
 shoppingList[1]="Banana";
 console.log("My shoppingList: ", shoppingList);
+
+favoriteFruits = ["grapefruit", "orange", "lemon"];
+favoriteFruits.push("tangerine");
+console.log(favoriteFruits);
+let lengthOfFavoriteFruits = favoriteFruits.push("lime");
+console.log(favoriteFruits);
+
+let arrOfShapes = ["circle", "triangle", "rectangle", "pentagon"];
+arrOfShapes.splice(2, 0, "square", "trapezoid");
+console.log(arrOfShapes);
+arrOfShapes.splice(2, 12, "rectangular");
+console.log(arrOfShapes);
+
+let arr5 = [1, 2, 3];
+let arr6 = [4, 5, 6];
+let arr7 = arr5.concat(arr6);
+console.log(arr7);
+let arr8 = arr7.concat(7, 8, 9);
+console.log(arr8);
+
+arr8.pop();
+console.log(arr8);
+
+arr8.shift();
+console.log(arr8);
+
+arr8.splice(1, 3);
+console.log(arr8);
+
+delete arr8[2];
+console.log(arr8);
+
+arr8 = [ 2, 6, 7, 8, 6, 6];
+let findIndex = arr8.indexOf(6);
+let findIndex2 = arr8.indexOf(10);
+console.log(findIndex, findIndex2);
+console.log(arr8.indexOf(6, 2));
+console.log(arr8.lastIndexOf(6));
+
+let ages = [18, 72, 33, 56, 40];
+console.log(ages);
+ages.sort();
+console.log(ages);
+let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+console.log(names);
+names.sort();
+console.log(names);
+names.reverse();
+console.log(names);
